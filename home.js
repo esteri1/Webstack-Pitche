211958e5cb180885b16746b7d87fe7c1165454f3
Esther <106969347+esteri1@users.noreply.github.com>
2023-07-21 08:56:09 +0100

    Update home.js

--- home.js
+++ home.js
@@ -82,10 +82,10 @@ window.onscroll = function() {
     // make navbar fixed & change logo color
     if (window.pageYOffset > headerSection.offsetHeight - 75) {
         nav.classList.add("active");
-        logoImage.src = "icon14.png";
+        logoImage.src = "icon15.png";
     } else {
         nav.classList.remove("active");
-        logoImage.src = "icon14.png";
+        logoImage.src = "icon15.png";
     }
 
     // header welcome fade out and in
